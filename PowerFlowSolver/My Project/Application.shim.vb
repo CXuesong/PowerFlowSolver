58e2a78c7c92f4d84806dca28fc196be303af9c7
forest93 <forest93@163.com>
2020-08-09 17:55:20 +0800

    迁移PowerFlowSolver至.NET 5。

---
+++ PowerFlowSolver/My Project/Application.shim.vb
@@ -0,0 +1,31 @@
+﻿Imports System.ComponentModel
+Imports CXuesong.Shims.VisualBasic.ApplicationServices
+Imports CXuesong.Shims.VisualBasic.Devices
+
+Namespace My
+
+    <HideModuleName>
+    Friend Module MyProject
+
+        Public ReadOnly Property Application As New MyApplication
+
+        Public ReadOnly Property Computer As New MyComputer
+
+    End Module
+
+    <EditorBrowsable(EditorBrowsableState.Never)>
+    Friend Class MyApplication
+        Inherits ConsoleApplicationBase
+
+        Public Sub New()
+            MyBase.New(GetType(MyApplication).Assembly)
+        End Sub
+    End Class
+
+    <EditorBrowsable(EditorBrowsableState.Never)>
+    Friend Class MyComputer
+        Inherits Computer
+
+    End Class
+
+End Namespace
